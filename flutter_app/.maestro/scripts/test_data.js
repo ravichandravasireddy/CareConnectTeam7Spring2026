8dc84edac614f8b571a0b43ff0a1a4060a740e05
Iamniquey <iamnique@gmail.com>
2026-02-13 15:35:38 -0500

    Complete patient flows for flutter app with maestro

--- flutter_app/.maestro/scripts/test_data.js
+++ flutter_app/.maestro/scripts/test_data.js
@@ -13,9 +13,10 @@ output.data = {
   CAREGIVER_EMAIL: "caregiver@careconnect.demo",
   CAREGIVER_PASSWORD: "password123",
   // === Health log test data
-  TEST_MOOD_VALUE: "😊 Happy",
-  TEST_BP_SYSTOLIC: "120",
-  TEST_BP_DIASTOLIC: "80",
+  TEST_MOOD_EMOJI: "🤩",
+  TEST_MOOD_VALUE: "Ecstatic",
+  TEST_BP_SYSTOLIC: "119",
+  TEST_BP_DIASTOLIC: "79",
   TEST_HEART_RATE: "72",
   // === Note test data
   TEST_NOTE_TITLE: "Medication Side Effects",
